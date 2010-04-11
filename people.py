67756da4d568948969c763cdc7b044e44a9d6d55
Matthew Hooker <mhooker@digg.com>
2010-04-10 22:26:47 -0400

    adding people object

---
+++ people.py
@@ -0,0 +1,23 @@
+"""
+author: Matthew Hooker (mwhooker@gmail.com)
+"""
+import random
+
+
+class People(object):
+    """Collect a list of people and expose a method to randomly select one"""
+
+    def __init__(self, api_handle):
+        """initialize with people"""
+        self.people = list()
+        self.api = api_handle
+        self.find_people()
+
+    def find_people(self):
+        """clear current people and find others"""
+        del self.people[:]
+        self.people = list(self.api.get_people())
+
+    def get_random(self):
+        """get a random person"""
+        return random.choice(self.people)
