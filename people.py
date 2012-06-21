7df423d0b02c718544f0ca9e770675b10388a8fa
Matthew H. <mwhooker@gmail.com>
2012-06-20 19:27:49 -0700

    such a newbie.

--- people.py
+++ people.py
@@ -9,20 +9,12 @@ class People(object):
 
     def __init__(self, api_handle):
         """initialize with people"""
-        self.people = list()
         self.api = api_handle
-        self.find_people()
-
-    def find_people(self):
-        """clear current people and find others"""
-        del self.people[:]
-        self.people = list(self.api.get_people())
+        self.people = self.api.get_people()
 
     def get_random(self):
         """get a random person"""
-        if not self.people:
-            self.find_people()
-
+        
         assert len(self.people) > 0
 
         return random.choice(self.people)
